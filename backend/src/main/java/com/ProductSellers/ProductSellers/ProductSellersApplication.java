8ec72ce3f0e1708430cc95f84790a50e08349ae3
Jeliel <jelielbraz71@gmail.com>
2022-12-18 00:30:17 -0300

    add spring

---
+++ backend/src/main/java/com/ProductSellers/ProductSellers/ProductSellersApplication.java
@@ -0,0 +1,13 @@
+package com.ProductSellers.ProductSellers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSellersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSellersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ProductSellers/ProductSellers/ProductSellersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProductSellers.ProductSellers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSellersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
